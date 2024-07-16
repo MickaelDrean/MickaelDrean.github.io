4309e2c7dcb765d1c14dd09bb62e2d82cd946655
MickaelDrean <mickael.drean@gmail.com>
2024-07-16 09:32:15 +0000

    Update from https://github.com/MickaelDrean/VivenciApp/commit/df0aca79db9b8348ddd112d6fc7b30f55d71a9d0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4047826454"
+    serviceWorkerVersion: "3177947398"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,7 +19,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "4c760115bd841a602e441ea349fa3
 "canvaskit/skwasm.js": "5d4f9263ec93efeb022bb14a3881d240",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "8fd4b8f6d2b2d26921b4d1c79af9f072",
+"flutter_bootstrap.js": "ab6089f04f5b70e55190ea613ba6ba27",
 "version.json": "af34a2d7b3f0a6a4c310b49d497b5b9c",
 "favicon.png": "d0239789fe665a20ba5516876aa6dd6d",
 "assets/packages/wakelock_plus/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
@@ -55,9 +55,9 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "4c760115bd841a602e441ea349fa3
 "assets/fonts/MaterialIcons-Regular.otf": "cf4d86fa6d84b81354e08d5aaea9008d",
 "assets/AssetManifest.json": "f5cb600b12f33e25e430f56fc64413f4",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"index.html": "9aabf9221b5621632963341b1aee1e7a",
-"/": "9aabf9221b5621632963341b1aee1e7a",
-"main.dart.js": "689c7a65dcfc9e18aa625e14a240b636"};
+"index.html": "64b35c50b6f6f3d695df0322ea9bc3b0",
+"/": "64b35c50b6f6f3d695df0322ea9bc3b0",
+"main.dart.js": "78e8af57a7c6571ea9815c995813266d"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
