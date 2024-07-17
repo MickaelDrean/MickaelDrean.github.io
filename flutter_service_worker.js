60833ec9174a978e7b9b009a0fbbd37db72b39cf
MickaelDrean <mickael.drean@gmail.com>
2024-07-17 13:58:29 +0000

    Update from https://github.com/MickaelDrean/VivenciApp/commit/fac5a3a3b97bab0b67d0a3405f7c6ef52386817b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,7 +19,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "4c760115bd841a602e441ea349fa3
 "canvaskit/skwasm.js": "5d4f9263ec93efeb022bb14a3881d240",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "842a277a7400920f10a1d34af0c78f31",
+"flutter_bootstrap.js": "e6cf4e671d836bd240df553674bc71ec",
 "version.json": "af34a2d7b3f0a6a4c310b49d497b5b9c",
 "favicon.png": "d0239789fe665a20ba5516876aa6dd6d",
 "assets/packages/wakelock_plus/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
@@ -52,12 +52,12 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "4c760115bd841a602e441ea349fa3
 "assets/assets/json/CatalogueMED-240531161808.json": "85f0aa7fa7fdd4e2f1d67869985c7292",
 "assets/assets/json/sample.json": "1df72fe9166a8d20a6b8addf525a0fb0",
 "assets/assets/english.png": "015a5fdbb9c32f341aac4d2084c462a0",
-"assets/fonts/MaterialIcons-Regular.otf": "ad225836da920198182fbc7ec14ef96f",
+"assets/fonts/MaterialIcons-Regular.otf": "cf4d86fa6d84b81354e08d5aaea9008d",
 "assets/AssetManifest.json": "f5cb600b12f33e25e430f56fc64413f4",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"index.html": "4470dd5737da9516bb478495a294f120",
-"/": "4470dd5737da9516bb478495a294f120",
-"main.dart.js": "05fe91e4b53998455cdc33eab96e8c4b"};
+"index.html": "7488a30ac09a869c3dbd2bd8d02afe74",
+"/": "7488a30ac09a869c3dbd2bd8d02afe74",
+"main.dart.js": "2cf56a830a476ef527d3f4efe35aff1e"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
