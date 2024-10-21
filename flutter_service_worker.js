2f80d80e971fc780d18d1154e802f4bb7d35f4e5
MickaelDrean <mickael.drean@gmail.com>
2024-10-21 15:47:42 +0000

    Update from https://github.com/MickaelDrean/VivenciApp/commit/8da7545bd4b40b25427024aab08daeb8e248ede7

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "assets/assets/portugese.png": "43f2ff9e7759db969836d582ddb124d6",
 "assets/assets/spanish.png": "f54a4d62d5f0b70d478170d636ca6f3d",
 "assets/assets/cfg/app_settings.json": "539b484768d58e63c813e8d4c44a7b77",
-"assets/assets/cfg/hash.txt": "ee719a2c4e4facd2f0623ad3509dbe32",
+"assets/assets/cfg/hash.txt": "dc27acdae0ae0ceb0151e2b1dea5d8b3",
 "assets/assets/danse_icon.png": "86f412b277e052b60e721ba03ddbee13",
 "assets/assets/logo.png": "b0842a081da65c324a122a7e1d61deb1",
 "assets/NOTICES": "2cda09024af9c286200f47fd44058fd2",
@@ -63,7 +63,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "4c760115bd841a602e441ea349fa37e0",
 "favicon.png": "d0239789fe665a20ba5516876aa6dd6d",
 "version.json": "af34a2d7b3f0a6a4c310b49d497b5b9c",
-"flutter_bootstrap.js": "60c3e0b52e907e924124d4fcabd4b10d"};
+"flutter_bootstrap.js": "4dc5503ece507ee4b9b6a347b144b67e"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
