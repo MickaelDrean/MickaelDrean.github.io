46802d7f59c3ae541a2594047d2c942edd410d1b
MickaelDrean <mickael.drean@gmail.com>
2024-11-21 10:36:02 +0000

    Update from https://github.com/MickaelDrean/VivenciApp/commit/a58ccbbca264446736de9d93f8945dcc4e8a33d1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "110c039d95f7ce4ca6d3ba3d5e18a771",
+"main.dart.js": "473806a82f2bc454c5ff81bc4b1d1004",
 "assets/FontManifest.json": "7df10702a8c60a62e6694f43081d46e7",
 "assets/AssetManifest.bin": "ada3a528dd60ce36d84326e2c0dbd677",
 "assets/fonts/MaterialIcons-Regular.otf": "07292696d4d72377a965700e67795976",
@@ -40,10 +40,10 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "assets/assets/portugese.png": "43f2ff9e7759db969836d582ddb124d6",
 "assets/assets/spanish.png": "f54a4d62d5f0b70d478170d636ca6f3d",
 "assets/assets/cfg/app_settings.json": "977a467f3c8c3e641b071c9008f37b1e",
-"assets/assets/cfg/hash.txt": "f793a912a9b9ed8c3bb363a512b8859d",
+"assets/assets/cfg/hash.txt": "9dd492cd086e98fd4d69b703a2b2955e",
 "assets/assets/danse_icon.png": "86f412b277e052b60e721ba03ddbee13",
 "assets/assets/logo.png": "b0842a081da65c324a122a7e1d61deb1",
-"assets/NOTICES": "2cda09024af9c286200f47fd44058fd2",
+"assets/NOTICES": "b37550ece1826d7435471380a50f69db",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.json": "b111be91f688a99df60ea1c6aea8c19e",
 "assets/AssetManifest.bin.json": "9c1ac9a4a25708756884399fa124a4bb",
@@ -66,7 +66,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "4c760115bd841a602e441ea349fa37e0",
 "favicon.png": "d0239789fe665a20ba5516876aa6dd6d",
 "version.json": "af34a2d7b3f0a6a4c310b49d497b5b9c",
-"flutter_bootstrap.js": "6aeb04a999d286a55c7b57428000bfe2"};
+"flutter_bootstrap.js": "04781042482bffed6ad169e48cc883cd"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
