615d5404989a117f1b4780b4a933b363832ee6ef
MickaelDrean <mickael.drean@gmail.com>
2025-09-07 11:24:29 +0000

    Update from https://github.com/MickaelDrean/VivenciApp/commit/3e8cd12ab91033f139a6903bfd2fbdc052fb9b64

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4158721598"
+    serviceWorkerVersion: "1735359804"
   }
 });
