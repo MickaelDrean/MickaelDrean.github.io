0486e0a4f2305b55a586e73b3d1a58a5c2431209
MickaelDrean <mickael.drean@gmail.com>
2025-12-09 21:12:06 +0000

    Update from https://github.com/MickaelDrean/VivenciApp/commit/1f3731c90de850170fb0f761ff339adf896f07d0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -74,7 +74,7 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "assets/assets/logo.png": "aa97a13df4ef89389d36d2416475c688",
 "assets/assets/logo.ico": "d78d77f0629f4bbb936daa5de178b2d6",
 "assets/assets/spanish.png": "f54a4d62d5f0b70d478170d636ca6f3d",
-"assets/assets/cfg/hash.txt": "9661e2e7f2c4aa1e1191fea049d0475f",
+"assets/assets/cfg/hash.txt": "2035d42dbf2df37c0074c4afeada76e7",
 "assets/assets/cfg/app_settings.json": "f5cbc476c603bd6540f760f3a8142f43",
 "assets/assets/help/en/troubleshooting.md": "ca1b88cfd4b09e2e83607789bb36a294",
 "assets/assets/help/en/gettingstarted.md": "1204e88a66e7bec8b85ed5b9c6545c61",
@@ -324,9 +324,9 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "d0239789fe665a20ba5516876aa6dd6d",
-"flutter_bootstrap.js": "6b5bfd74d8cdfaf6a8f90ab60f0939bc",
+"flutter_bootstrap.js": "654b3e8fe1301ea01894aefa985d30c9",
 "version.json": "d9327004768d3e7ac7c66932d2b26843",
-"main.dart.js": "1713356958ba000900d1dc93c5d84fb3"};
+"main.dart.js": "cbde8e1d4298523f0a8e8056709222cf"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
