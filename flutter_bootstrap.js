3ec1d8e416591516cf053bf03561445f9ebbcd64
MickaelDrean <mickael.drean@gmail.com>
2025-12-18 11:32:38 +0000

    Update from https://github.com/MickaelDrean/VivenciApp/commit/a185aa3f3b595de06e2df1cf6780adc772712213

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "554439293"
+    serviceWorkerVersion: "4147478800"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -74,7 +74,7 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "assets/assets/logo.png": "aa97a13df4ef89389d36d2416475c688",
 "assets/assets/logo.ico": "d78d77f0629f4bbb936daa5de178b2d6",
 "assets/assets/spanish.png": "f54a4d62d5f0b70d478170d636ca6f3d",
-"assets/assets/cfg/hash.txt": "2d12a9a2e5fb5e6b662f5c85b9fb1423",
+"assets/assets/cfg/hash.txt": "e6ba3932d7be872244bcf8bb9f627ed7",
 "assets/assets/cfg/app_settings.json": "f5cbc476c603bd6540f760f3a8142f43",
 "assets/assets/help/en/troubleshooting.md": "ca1b88cfd4b09e2e83607789bb36a294",
 "assets/assets/help/en/gettingstarted.md": "1204e88a66e7bec8b85ed5b9c6545c61",
@@ -324,9 +324,9 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "d0239789fe665a20ba5516876aa6dd6d",
-"flutter_bootstrap.js": "45dbd5ad84e7edbe7fcfa13e98193fb7",
+"flutter_bootstrap.js": "cd068bb755f397307ceb8d56e29af9e6",
 "version.json": "d9327004768d3e7ac7c66932d2b26843",
-"main.dart.js": "ca19f01dcf6620c8950a865ccb48ad28"};
+"main.dart.js": "3571d4deefdf2ccb5456b5a13810f5f0"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
